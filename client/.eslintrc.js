27f4bc3df72d7dc06b1a77d9e52230267006bc5b
Mohamed Dabbagh <mohamed.dabbagh@slalom.com>
2021-09-07 14:06:59 -0400

    Adding prettier and eslint integration.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     jest: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
