d0330d04b337f77703757b824ad78e1ff4033d6a
Mohamed Dabbagh <mohamed.dabbagh@slalom.com>
2021-09-09 22:50:50 -0400

    Adding dotenv and moving database config to .env.

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: "testing123",
-  host: "localhost",
-  database: "perntodo",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
